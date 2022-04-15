ae35d18c03eccaa908a28630de14e1c4e0c09c4b
your name <your email>
2022-04-15 08:33:24 +0800

    SpringBoot.

--- src/main/java/com/bdqn/Application.java
+++ src/main/java/com/bdqn/Application.java
@@ -3,13 +3,21 @@ package com.bdqn;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
 @MapperScan("com.bdqn.dao")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class);
